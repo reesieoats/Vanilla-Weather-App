4b625779c8f1d93440eb51df5b1be1eb2d1a59d9
reesieoats <reesieezy@gmail.com>
2020-07-29 19:19:46 -0400

    added app.js api key and url

--- src/app.js
+++ src/app.js
@@ -1,1 +1,4 @@
-alert("Hello World");
+let apiKey = "595adbf6a3ae27cee86c95730ea72e7a";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}`;
+
+console.log(apiUrl);
