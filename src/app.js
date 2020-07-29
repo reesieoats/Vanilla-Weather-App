7c21551b91ce7eecfed47358026e25b78d86a452
reesieoats <reesieezy@gmail.com>
2020-07-29 19:57:36 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
 let apiKey = "595adbf6a3ae27cee86c95730ea72e7a";
-let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=New York&appid=595adbf6a3ae27cee86c95730ea72e7a`;
+let apiUrl =
+  "http://api.openweathermap.org/data/2.5/weather?q=Cleveland&appid=595adbf6a3ae27cee86c95730ea72e7a";
 console.log(apiUrl);
