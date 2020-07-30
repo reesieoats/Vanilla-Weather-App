b14fa3fec2cdf12afeafc05586f257d9112af7fd
reesieoats <reesieezy@gmail.com>
2020-07-30 17:41:15 -0400

    added js for fahrenheit link

--- src/app.js
+++ src/app.js
@@ -58,7 +58,15 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  alert("link clicked");
+}
+
 search("New York");
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
