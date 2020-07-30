0fb473ae2c0597942660b3ce5afa9ea826eadd27
reesieoats <reesieezy@gmail.com>
2020-07-29 21:55:07 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
   let minutes = date.getMinutes();
-  if minutes < 10) {
-      minutes = `0${minutes}`;
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
   }
   let days = [
     "Sunday",
