3b313315262ca4a2b400fd09ee2225d622531f14
reesieoats <reesieezy@gmail.com>
2020-07-29 20:36:00 -0400

    add description element to js

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
 }
 
 let apiKey = "595adbf6a3ae27cee86c95730ea72e7a";
