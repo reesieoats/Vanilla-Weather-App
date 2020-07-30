80ec62170f103e682209f2f2623617c955271d16
reesieoats <reesieezy@gmail.com>
2020-07-29 21:21:15 -0400

    added date html and js

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("date");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
