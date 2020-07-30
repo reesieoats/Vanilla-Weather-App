ad5bb67eb272e90c98e5c7ea3dd98617c6ff585f
reesieoats <reesieezy@gmail.com>
2020-07-29 20:00:06 -0400

    update units

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 let apiKey = "595adbf6a3ae27cee86c95730ea72e7a";
 let apiUrl =
-  "http://api.openweathermap.org/data/2.5/weather?q=Cleveland&appid=595adbf6a3ae27cee86c95730ea72e7a";
+  "http://api.openweathermap.org/data/2.5/weather?q=Cleveland&appid=595adbf6a3ae27cee86c95730ea72e7a&units=metric";
+
 console.log(apiUrl);
