23b2dc4b7ed5af9bf8daee07c9f3da88683b3c71
reesieoats <reesieezy@gmail.com>
2020-07-29 21:12:14 -0400

    updated wind html and js

--- src/app.js
+++ src/app.js
@@ -4,11 +4,13 @@ function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "595adbf6a3ae27cee86c95730ea72e7a";
