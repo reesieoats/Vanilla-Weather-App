2ba2ea84148c5fd1712109dc9d1293b77bb7c552
reesieoats <reesieezy@gmail.com>
2020-07-29 20:47:09 -0400

    added humidity js and html

--- src/app.js
+++ src/app.js
@@ -3,9 +3,12 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
 }
 
 let apiKey = "595adbf6a3ae27cee86c95730ea72e7a";
