48bc4b62a45096e6455e046cefe1faffc7def140
reesieoats <reesieezy@gmail.com>
2020-07-29 21:56:47 -0400

    updated real time hours in js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
   let minutes = date.getMinutes();
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
