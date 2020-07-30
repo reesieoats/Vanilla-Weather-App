2f382739a6c9606611daf6df7b6987cdb72b0d62
reesieoats <reesieezy@gmail.com>
2020-07-29 20:26:32 -0400

    added temperature display in html and js function

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = response.data.main.temp;
 }
 
 let apiKey = "595adbf6a3ae27cee86c95730ea72e7a";
