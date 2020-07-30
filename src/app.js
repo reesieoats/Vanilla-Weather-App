b6597cf45aa7967f8e464265fbedfd85c3d9bebf
reesieoats <reesieezy@gmail.com>
2020-07-30 14:49:57 -0400

    added https to link

--- src/app.js
+++ src/app.js
@@ -42,7 +42,7 @@ function displayTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/10d@2x.png`
+    `https://openweathermap.org/img/wn/10d@2x.png`
   );
 }
 
