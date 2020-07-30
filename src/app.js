2062175200baa349a0a98e448358a82bfede1608
reesieoats <reesieezy@gmail.com>
2020-07-30 15:38:47 -0400

    adjusted search engine js and html

--- src/app.js
+++ src/app.js
@@ -46,8 +46,17 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+function search(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  console.log(cityInputElement);
+}
+
 let apiKey = "595adbf6a3ae27cee86c95730ea72e7a";
 let city = "New York";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=595adbf6a3ae27cee86c95730ea72e7a&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
