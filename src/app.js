8c2ac17032c1943e4b0e3e9ae22db48373a9abbc
reesieoats <reesieezy@gmail.com>
2020-07-29 20:06:15 -0400

    update app.js with function and axios

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
+function displayTemperature(response) {
+  console.log(response.data);
+}
+
 let apiKey = "595adbf6a3ae27cee86c95730ea72e7a";
 let apiUrl =
   "http://api.openweathermap.org/data/2.5/weather?q=Cleveland&appid=595adbf6a3ae27cee86c95730ea72e7a&units=metric";
 
-console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
