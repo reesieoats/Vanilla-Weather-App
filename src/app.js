5306ba6938cbd32c40007ecb96951c6857ac1bc9
reesieoats <reesieezy@gmail.com>
2020-07-30 15:48:41 -0400

    removed console.log added search value

--- src/app.js
+++ src/app.js
@@ -55,7 +55,7 @@ function search(city) {
 function handleSubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
-  console.log(cityInputElement.value);
+  search(cityInputElement.value);
 }
 
 search("New York");
