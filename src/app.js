3213effac5f0ba3770cb2f384d9aa57779f2b67f
reesieoats <reesieezy@gmail.com>
2020-07-29 21:25:40 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,14 +5,14 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
-  let dateElement = document.querySelector("date");
+  let dateElement = document.querySelector("#date");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  dateElement.innerHTML = response.data.coord.dt;
+  dateElement.innerHTML = "Friday 5:00";
 }
 
 let apiKey = "595adbf6a3ae27cee86c95730ea72e7a";
