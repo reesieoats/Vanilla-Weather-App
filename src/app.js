94a39c59d99210bdbb68f444f047640a21464ebe
reesieoats <reesieezy@gmail.com>
2020-08-03 12:02:09 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -73,6 +73,21 @@ function displayForecast(response) {
               </div>
             </div>`;
 
+  forecast = response.data.list[1];
+  forecastElement.innerHTML += ` <div class="col-2">
+              <h1>
+                ${formatHours(forecast.dt * 1000)}
+              </h1>
+              <img src="https://openweathermap.org/img/wn/${
+                forecast.weather[0].icon
+              }@2x.png" />
+              <div class="weather-forcast-temperature">
+                <strong>${Math.round(
+                  forecast.main.temp_max
+                )}°</strong>${Math.round(forecast.main.temp_min)}°
+              </div>
+            </div>`;
+
   forecast = response.data.list[2];
   forecastElement.innerHTML += ` <div class="col-2">
               <h1>
